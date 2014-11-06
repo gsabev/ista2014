4f0bbdfcf7b4d22d587be40b11a54614c5369625
Georgi Sabev <georgi.sabev@sap.com>
2014-11-06 17:29:59 +0200

    Step 1: Write tests to assert the current system behaviour. Bugs are logged, but not fixed yet. We want to wait a bit till we have more information

--- src/test/CacheRegisterTest.java
+++ src/test/CacheRegisterTest.java
@@ -1,9 +1,10 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import legacy.CacheRegister;
 import legacy.ItemRegistry;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,8 +23,69 @@ public class CacheRegisterTest {
 
 	@Test
 	public void testSuccesfulSales() {
-		register.sellItem(registry, "pen");
-		Assert.assertEquals(1, register.successfulSales());
+		boolean successfulSale = register.sellItem(registry, "pen");
+		assertTrue(successfulSale);
+		assertEquals(1, register.successfulSales());
 	}
 
+	@Test
+	public void testMultipleSuccessfulSales() {
+		boolean sale1 = register.sellItem(registry, "pen");
+		boolean sale2 = register.sellItem(registry, "pencil");
+		boolean sale3 = register.sellItem(registry, "rubber");
+		assertTrue(sale1 && sale2 && sale3);
+		assertEquals(3, register.successfulSales());
+	}
+
+	@Test
+	public void testSellSeveralOfOneType() {
+		boolean sale1 = register.sellItem(registry, "pen");
+		boolean sale2 = register.sellItem(registry, "pen");
+		// TODO : Fix bug CR-325 (logged)
+		// It should be possible to sell two pens
+
+		// assertTrue(sale1 && sale2);
+		// assertEquals(2, register.successfulSales());
+		assertEquals(true, sale1);
+		assertEquals(false, sale2);
+		assertEquals(1, register.successfulSales());
+	}
+
+	@Test
+	public void testReturnItem() {
+		register.sellItem(registry, "defective pen");
+		boolean successfulReturn = register.returnItem(registry,
+				"defective pen");
+		assertTrue(successfulReturn);
+		assertEquals(1, register.returnedItems());
+	}
+
+	@Test
+	public void testReturnNoSales() {
+		boolean successfulReturn = register.returnItem(registry, "pen");
+		assertEquals(false, successfulReturn);
+		// TODO : fix bug CR-345 (logged). Unsuccessful returns should
+		// not be counted
+
+		// assertEquals(0, register.returnedItems());
+		assertEquals(1, register.returnedItems());
+	}
+
+	@Test
+	public void testUnsuccessfulReturn() {
+		register.sellItem(registry, "pen");
+		boolean success = register.returnItem(registry, "bird");
+		assertEquals(false, success);
+		// TOTO : Bug CR-345 (see above)
+		// assertEquals(0, register.returnedItems());
+		assertEquals(1, register.returnedItems());
+	}
+
+	@Test
+	public void testTotalSales() {
+		register.sellItem(registry, "pen");
+		register.sellItem(registry, "pencil");
+		register.returnItem(registry, "pen");
+		assertEquals(2, register.totalSales());
+	}
 }
